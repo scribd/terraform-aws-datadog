6584ce7d3318a2c9323b26d2ed1312f4a645acce
Jim Park <jimp@scribd.com>
2019-09-19 14:44:47 -0700

    fetch the account ID used for writing logs

--- logshipping_elb.tf
+++ logshipping_elb.tf
@@ -18,10 +18,39 @@ resource "aws_s3_bucket_notification" "elblog-notification-dd-log" {
   }
 }
 
+data "aws_elb_service_account" "main" {}
 
 resource "aws_s3_bucket" "elb_logs" {
   bucket = "scribd-${var.namespace}-elb-logs"
   acl    = "private"
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::scribd-${var.namespace}-elb-logs/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.main.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+       sse_algorithm = "AES256"
+      }
+    }
+  }
 
   lifecycle_rule {
     id      = "log"
