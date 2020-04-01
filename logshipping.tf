571a51751774f3cbfb7f813d71399944eaad8230
Jim Park <jimp@scribd.com>
2020-03-31 20:14:45 -0700

    change DD_ENHANCED_METRICS to false to enable the layer to work correctly with log forwarder

--- logshipping.tf
+++ logshipping.tf
@@ -97,9 +97,10 @@ resource "aws_lambda_function" "dd-log" {
 
   environment {
     variables = {
-      DD_API_KEY       = var.datadog_api_key
-      DD_TAGS          = "namespace:${var.namespace},env:${var.env}"
-      EXCLUDE_AT_MATCH = var.log_exclude_at_match
+      DD_API_KEY          = var.datadog_api_key
+      DD_ENHANCED_METRICS = "false"
+      DD_TAGS             = "namespace:${var.namespace},env:${var.env}"
+      EXCLUDE_AT_MATCH    = var.log_exclude_at_match
     }
   }
 }
