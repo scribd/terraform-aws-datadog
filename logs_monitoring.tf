647e8e9dd8d8d0dc953f64c86fed25ceebf1fead
Jim Park <jimp@scribd.com>
2020-04-02 18:33:40 -0700

    fix: use AWS Secrets Manager instead of supplying API Key parameter
    - Avoids always reapplying changes due to this bug: https://github.com/terraform-providers/terraform-provider-aws/issues/55

--- logs_monitoring.tf
+++ logs_monitoring.tf
@@ -1,11 +1,27 @@
+data "local_file" "template_yaml" {
+  filename = "${path.module}/logs_monitoring_template.yaml"
+}
+
 resource "aws_cloudformation_stack" "datadog-forwarder" {
   name         = "datadog-forwarder"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters = {
-    DdApiKey       = var.datadog_api_key
+    DdApiKeySecret = aws_secretsmanager_secret.datadog_api_key.arn
     DdTags         = "namespace:${var.namespace},env:${var.env}"
     ExcludeAtMatch = var.log_exclude_at_match
     FunctionName   = "datadog-forwarder"
   }
-  template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/3.6.0.yaml"
+  #template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/3.6.0.yaml"
+  template_body = data.local_file.template_yaml.content
+
+}
+
+resource "aws_secretsmanager_secret" "datadog_api_key" {
+  name        = "datadog_api_key"
+  description = "Datadog API Key"
+}
+
+resource "aws_secretsmanager_secret_version" "datadog_api_key" {
+  secret_id     = aws_secretsmanager_secret.datadog_api_key.id
+  secret_string = var.datadog_api_key
 }
