ffa56b80f2784abbf8d67f7fc828e522624b72f3
Qingping Hou <qph@scribd.com>
2020-04-07 11:47:18 -0700

    remove redundant dash in forwarder resource names

--- logs_monitoring.tf
+++ logs_monitoring.tf
@@ -3,13 +3,13 @@ data local_file "dd_forwarder_cf_template" {
 }
 
 resource aws_cloudformation_stack "datadog-forwarder" {
-  name         = "${local.stack_prefix}-datadog-forwarder"
+  name         = "${local.stack_prefix}datadog-forwarder"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters = {
     DdApiKeySecret = aws_secretsmanager_secret.datadog_api_key.arn
     DdTags         = "namespace:${var.namespace},env:${var.env}"
     ExcludeAtMatch = var.log_exclude_at_match
-    FunctionName   = "${local.stack_prefix}-datadog-forwarder"
+    FunctionName   = "${local.stack_prefix}datadog-forwarder"
   }
   #template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/3.6.0.yaml"
   template_body = data.local_file.dd_forwarder_cf_template.content
@@ -17,7 +17,7 @@ resource aws_cloudformation_stack "datadog-forwarder" {
 }
 
 resource aws_secretsmanager_secret "datadog_api_key" {
-  name        = "${local.stack_prefix}-datadog_api_key"
+  name        = "${local.stack_prefix}datadog_api_key"
   description = "Datadog API Key"
   tags        = local.default_tags
 }
