baede43eb21b75b740e551a19863fcb8df690a52
Artem Kajalainen <artem.kajalainen@gofore.com>
2020-06-24 15:04:47 +0300

    fix datadog-forwarder CF stack params and ignore DdApiKey param

--- logs_monitoring.tf
+++ logs_monitoring.tf
@@ -2,13 +2,19 @@ resource aws_cloudformation_stack "datadog-forwarder" {
   name         = "${local.stack_prefix}datadog-forwarder"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters   = {
-    DdApiKeySecret = aws_secretsmanager_secret.datadog_api_key.arn
-    DdApiKey       = "dummy-value"
-    DdTags         = "namespace:${var.namespace},env:${var.env}"
-    ExcludeAtMatch = var.log_exclude_at_match
-    FunctionName   = "${local.stack_prefix}datadog-forwarder"
+    DdApiKeySecretArn = aws_secretsmanager_secret.datadog_api_key.arn
+    DdApiKey          = "dummy-value"
+    DdTags            = "namespace:${var.namespace},env:${var.env}"
+    ExcludeAtMatch    = var.log_exclude_at_match
+    FunctionName      = "${local.stack_prefix}datadog-forwarder"
   }
   template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/${var.dd_forwarer_template_version}.yaml"
+
+  lifecycle {
+    ignore_changes = [
+      parameters["DdApiKey"]
+    ]
+  }
 }
 
 resource aws_secretsmanager_secret "datadog_api_key" {
