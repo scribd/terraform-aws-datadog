7240d04b0b303948490d7eb099906e8370dfbc35
Jim Park <jimp@scribd.com>
2020-11-06 09:58:45 -0800

    Revert "fix: use proper provider source for datadog"
    This reverts commit 6f292e7d0b2c277c9751dce1c9806c47b1f22b89.
    
    - This change to be backed out of the v1 major release as it breaks
      support for Terraform 0.12.x

--- versions.tf
+++ versions.tf
@@ -2,9 +2,6 @@ terraform {
   required_version = ">= 0.12, < 0.14"
 
   required_providers {
-    datadog = {
-      source  = "DataDog/datadog"
-      version = ">= 2.10, < 3"
-    }
+    datadog = ">= 2.10, < 3"
   }
 }
